4909bcc5e0ef23202db8655c88614ef60e2cf00a
kadelcode <kadelcode@gmail.com>
2022-10-02 13:17:21 +0100

    Adding an inline function

--- Functions/functions.cpp
+++ Functions/functions.cpp
@@ -8,9 +8,15 @@ int a,b,c;
 // function declaration
 int sum(int ,int );
 
+// inline function
+inline int square(int s) {
+    return s * s;
+}
+
 int main() {
     // invoking the function
-    cout << sum(30,40);
+    cout << sum(30,40) << "\n";
+    cout << square(2);
     return 0;
 }
 
